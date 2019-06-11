60418e44961e9f28516ff6474fbdd6658895cc3e
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2019-06-11 13:43:00 +0200

    finalized login script

--- app/src/main/java/com/example/project_ss_b/MainActivity.java
+++ app/src/main/java/com/example/project_ss_b/MainActivity.java
@@ -3,15 +3,38 @@ package com.example.project_ss_b;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText name;
     EditText pass;
     TextView tView;
+
+    private TextView txtResponse;
+    private String jsonResponse;
+
+    // URL to get contacts JSON
+    private static String url = "https://api.androidhive.info/contacts/";
+
+    ArrayList<HashMap<String, String>> user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +46,97 @@ public class MainActivity extends AppCompatActivity {
         tView = findViewById(R.id.textView);
     }
 
-    public void Login(View view){
-        String username = name.getText().toString();
-        String password = pass.getText().toString();
+    //function that is called after response
+    private void responder(String response){
+        //try to create json object of the response
+        try{
+            JSONObject obj = new JSONObject(response);
 
-        String loginName = "admin";
-        String loginPass = "admin";
+            JSONObject user = obj.getJSONObject("user");
+            String name = user.getString("achternaam");
+            String email = user.getString("email");
+            String id = user.getString("id");
 
-        if(username.equals(loginName) && password.equals(loginPass)){
-            //Intent intent = new Intent(this, MenuActivity.class);
-            //startActivity(intent);
-        }else {
-            tView.setText("Gebruikersnaam of wachtwoord is verkeerd. Probeer het nog eens.");
+            System.out.println("HELP");
+
+            jsonResponse = "";
+            jsonResponse += "Name: " + name + "\n\n";
+            jsonResponse += "Email: " + email + "\n\n";
+            jsonResponse += "Id: " + id + "\n\n";
+
+            //txtResponse.setText(jsonResponse);
+
+            String toast;
+
+            System.out.println("NOPES");
+            if(!obj.getBoolean("error")){
+                toast = "succes: " + obj.getString("message");
+                System.out.println("GOOD");
+                //Intent intent = new Intent(this, MenuActivity.class);
+                //startActivity(intent);
+            }else {
+                toast = "Error: " + obj.getString("message");
+                System.out.println("ERROR");
+
+                tView.setText("Gebruikersnaam of wachtwoord is verkeerd. Probeer het nog eens.");
+            }
+
+            // a small message to notify the user what happened
+            Toast message = Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT);
+            message.show();
+        }catch (Throwable t) {
+            System.out.println(response + " was not json " + t);
         }
+    }
+
+    public void send(View view){
+
+        //get data from inputs my case not used :)
+        //EditText editEmail = findViewById(R.id.email);
+        final String email = name.getText().toString();
+        //EditText editPassword = findViewById(R.id.wachtwoord);
+        final String password = pass.getText().toString();
+
+        //create Volley queue
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        //url to API
+        String url ="http://145.24.222.132/api.php";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        responder(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+                System.out.println("That didn't work! " + error);
+            }
+        }
+        ){
+            @Override
+            protected Map<String, String> getParams()
+            {
+                //place the paremeters
+                Map<String, String>  params = new HashMap<String, String>();
+                //the function you want to call
+                params.put("function", "login");
+                //the arguments it needs read README for that
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+        System.out.println(email);
+        System.out.println(password);
     }
 }
